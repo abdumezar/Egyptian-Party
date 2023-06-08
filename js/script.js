e60f13a6283d357d35552fc2f0f6de11467f6576
Abdulrhman Mezar <77861179+abdumezar@users.noreply.github.com>
2023-06-08 15:27:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ $(".singer").click(function (e) {
 
 // Counter down
 // Set the date we're counting down to
-let countDownDate = new Date("Jan 20, 2023 21:17:00").getTime();
+let countDownDate = new Date("Jan 20, 2026 21:17:00").getTime();
 // Update the count down every 1 second
 let x = setInterval(function () {
   // Get today's date and time
